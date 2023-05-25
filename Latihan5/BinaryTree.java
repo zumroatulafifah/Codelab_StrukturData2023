a42959cfb9ea0f79d74e9d2b961fea0790d0c40f
Zumro'atul Afifah <111214043+zumroatulafifah@users.noreply.github.com>
2023-05-25 19:24:35 +0700

    Add files via upload

---
+++ Latihan5/BinaryTree.java
@@ -0,0 +1,45 @@
+package Latihan5;
+
+public class BinaryTree {
+    public Node root;
+    public void NewNode(int data){
+        root = NewNode(root, new Node(data));
+    }
+
+    private Node NewNode (Node root, Node newData){
+        if (root == null){
+            root = newData;
+            return root;
+        }
+        if(newData.data < root.data){
+            root.left = NewNode(root.left, newData);
+        }else{
+            root.right = NewNode(root.right, newData);
+        }
+        return root;
+    }
+
+    public void inOrder(Node node){
+        if(node != null){
+            inOrder(node.left);
+            System.out.println(node.data + " ");
+            inOrder(node.right);
+        }
+    }
+
+    public void preOrder(Node node){
+        if(node != null){
+            System.out.println(node.data + " ");
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void postOrder(Node node){
+        if(node != null){
+            postOrder(node.left);
+            postOrder(node.right);
+            System.out.println(node.data + " ");
+        }
+    }
+}
